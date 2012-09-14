2e87b131f3f71be4bd37bf1823f421004cfab071
Ruxton <Ruxton@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-14 09:00:53 +0000

    moving to 0.4 for deploy
    git-svn-id: https://plugins.svn.wordpress.org/gmep-oembed-support/trunk@598890 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gmep-oembed.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name:    GMEP oEmbed Support
+Plugin URI:     http://gmep.imeducate.com/contact
+Description:    Adds support for http://gmep.imeducate.com/ to the internal oEmbed parser
+Author:         The Frontier Group
+Author URI:     http://www.thefrontiergroup.com.au/
+Version:        0.4
+*/
+
+/*  Copyright 2012  The Frontier Group  (email : info@thefrontiergroup.com.au)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+function gmep_add_oembed_support() {
+  wp_oembed_add_provider('http://gmep.imeducate.com/*', 'http://gmep.imeducate.com/oembed');
+}
+add_filter('init', 'gmep_add_oembed_support');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== GMEP oEmbed Support ===
+Contributors: ruxton
+Tags: gmep, medical, medical education
+Requires at least: 3.4.0
+Tested up to: 3.4.1
+Stable tag: 0.4
+
+Adds support for http://gmep.imeducate.com/ to the internal oEmbed parser
+
+== Description ==
+
+This plugin adds oEmbed from http://gmep.imeducate.com/ to the WordPress oEmbed parsers internal whitelist.  Allowing users to embed media and profiles from the site.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Unzip the contents of the zip file
+2. Upload the `gmep-oembed` directory to the `/wp-content/plugins/` directory on your website
+3. Activate the plugin through the 'Plugins' menu in WordPress
+
+Any links to GMEP content that can embed now will, to force embeding you can wrap the link in [embed]linkhere[/embed]
